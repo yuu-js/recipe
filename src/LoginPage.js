3926ad48d0f638f97004eb7f40eff060b148ca07
yuu-js <wahyufirmandana9@gmail.com>
2024-08-13 11:39:28 +0700

    logpage updated

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -11,7 +11,63 @@ const Login = () => {
           <FoodWork />
         </div>
       </div>
-      <div className="flex-1 bg-white"></div>
+      <div className="w-1/2 flex items-center justify-center bg-white">
+        <form className="w-3/4 max-w-md">
+          <h2 className="text-2xl font-bold text-yellow-500 text-center mb-1">
+            Welcome
+          </h2>
+          <h1 className="text-1xl text-slate-500 text-center mb-7">
+            Login to your account
+          </h1>
+          <label
+            htmlfor="email"
+            className="block text-sm font-medium text-slate-600"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            placeholder="Example12@gmail.com"
+            className="border p-2 w-full mb-4"
+          />
+          <label
+            htmlfor="password"
+            className="block text-sm font-medium text-slate-600"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="example123"
+            className="border p-2 w-full mb-4"
+          />
+          <div className="flex items-center justify-between mb-4">
+            <label className="inline-flex items-center">
+              <input type="checkbox" className="form-checkbox accent-yellow-500" />
+              <span className="ml-2">Agree to terms & conditions</span>
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="bg-yellow-500 text-white py-2 px-4 w-full rounded"
+          >
+            Login
+          </button>
+          <div className="mt-2 text-center">
+            <p>
+              <a href="#" className="text-blue-500">
+                Forgot Password?
+              </a>
+            </p>
+            <p className="mt-5">
+              Don't have an account?{" "}
+              <a href="#" className="text-blue-500">
+                Sign up
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/element/foodworkImage.js
+++ src/components/element/foodworkImage.js
@@ -2,8 +2,8 @@ import foodWork from '../foodwork-topaz-denoise-enhance-6x-sharpen.jpeg';
 
 const FoodWork = () => {
   return (
-    <div className="p-0">
-      <img src={foodWork} alt="" className="" />
+    <div className="p-0 h-screen">
+      <img src={foodWork} alt="" className="w-full h-full object-cover" />
     </div>
   );
 };
